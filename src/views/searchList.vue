00e651927ea5bb6f095dc76d91f4ec8e066615a8
Beth113 <931534311@qq.com>
2025-10-10 23:04:48 +0800

    search-item

--- src/views/searchList.vue
+++ src/views/searchList.vue
@@ -2,10 +2,37 @@
 
 <template>
   <div class="searchList">
-    <div class="search-item">1</div>
-    <div class="search-item">2</div>
-    <div class="search-item">3</div>
-    <div class="search-item">4</div>
+    <div class="search-item">
+      <div class="search-box">
+        <label for="hotels-destination">目的地/酒店名称</label>
+        <input type="text" id="hotels-destination" placeholder="北京" />
+      </div>
+    </div>
+    <div class="search-item">
+      <div class="search-box">
+        <label for="">入住时间/退房时间</label>
+        <el-date-picker
+          v-model="value"
+          type="daterange"
+          start-placeholder="Start date"
+          end-placeholder="End date"
+          value-format="YYYY-MM-DD HH:mm:ss"
+          :default-time="defaultTime"
+        />
+      </div>
+    </div>
+    <div class="search-item">
+      <div class="search-box">
+        <label for="room-guest">房间及住客</label>
+        <input type="text" id="room-guest" placeholder="1间/1位" />
+      </div>
+    </div>
+    <div class="search-item">
+      <div class="search-box">
+        <label for="keyword">关键词（选填）</label>
+        <input type="text" id="keyword" placeholder="火车、酒店名称或区域" />
+      </div>
+    </div>
     <div class="search-item">5</div>
   </div>
 </template>
